fdfb65a183dbc46b78dc215716ec16cfa5060328
Abon Duttaroy <abondr.job@gmail.com>
2020-04-09 02:36:50 +0530

    develop migration 1

--- database/migrations/2020_04_08_155005_create_products_table.php
+++ database/migrations/2020_04_08_155005_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string("name");
+            $table->text("detail");
+            $table->decimal("price",12,2);
+            $table->bigInteger("stock");
+            $table->decimal("discount",12,2);
             $table->timestamps();
         });
     }
